2ca7ca95bd46799d8e2d4f0017f1464136e3b09d
wnsgk42 <57088799+wnsgk42@users.noreply.github.com>
2020-02-27 16:47:29 +0900

    fragment add

--- app/src/main/java/com/example/homeworkapp1/MainActivity.java
+++ app/src/main/java/com/example/homeworkapp1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.homeworkapp1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentName fragment_name = new FragmentName();
+        FragmentOld fragment_old = new FragmentOld();
+        FragmentBirth fragment_birth = new FragmentBirth();
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.frg_old, new FragmentOld());
+        fragmentTransaction.add(R.id.frg_bth, new FragmentBirth());
+        fragmentTransaction.add(R.id.frg_name, new FragmentName());
+        fragmentTransaction.commit();
+
+
     }
 }
